4b57c058cfcef3c78d6e26e9764848678b15df54
WhitexFlame <WhitexFlame@users.noreply.github.com>
2021-10-15 18:17:47 +0000

    iterating over arrays: Banana Bread

---
+++ lighthouse-js-fundamentals/ingredients.js
@@ -0,0 +1,48 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while look that prints out the contents of ingredients:
+
+// let i = 0;
+
+// while (i < ingredients.length) {
+//   console.log(ingredients[i]);
+//   i++;
+// }
+
+
+
+
+// Write a for loop that prints out the contents of ingredients:
+
+// for (i = 0; i < ingredients.length; i++) {
+//   console.log(ingredients[i]);
+// }
+
+
+
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+// for (let i = ingredients.length - 1; i >= 0; i--) {
+//   console.log(ingredients[i]);
+// }
+
+// logs: 
+// bananas
+// chocolate chips
+// baking powder
+// baking soda
+// sugar
+// flour
+// milk
+// eggs
+
+
+// let i = ingredients.length;
+
+// while (i--) {
+//   console.log(ingredients[i]);
+// }
+
+
+
